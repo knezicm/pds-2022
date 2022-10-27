ec98defee3bd7f2e4d7c18bfce628066ea82796a
Mladen Knezic <mladen.knezic@etf.unibl.org>
2022-10-27 14:56:07 +0200

    Updated actions workflow files

---
+++ run-test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "This pull request is opened by $PR_AUTHOR."
+
+DIR="./assignments/$PR_AUTHOR/"
+if [ -d "$DIR" ]; then
+  echo "Directory ${DIR} exists. Now checking if test.vhd file exists..."
+  cd $DIR
+else
+  echo "Error: ${DIR} not found. Aborting..."
+  exit 1
+fi
+
+if test -f test.vhd; then
+    echo "File test.vhd exists. Checking syntax..."
+    ghdl -s test.vhd
+else
+    echo "File test.vhd does not exist. Aborting..."
+    exit 1
+fi
