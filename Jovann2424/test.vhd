a8a8034acbe65841b9c88fbd8c657a4a29613d9d
Jovan Novakovic <73527927+Jovann2424@users.noreply.github.com>
2022-11-01 00:31:00 +0100

    Pokusa commit-Jovan-Novakovic

---
+++ Jovann2424/test.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity test is
+port (
+
+	input  : in std_logic_vector (3 downto 0);
+	sel    : in std_logic_vector (1 downto 0);
+	output : out std_logic;
+	);
+end test;
+
+architecture test_arch of test is
+
+begin
+
+end test_arch;
