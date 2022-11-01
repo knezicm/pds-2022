e1be30d50ec491a28d7dd2cc9a008ca36f4332d4
Nemanja Cenic <59769533+Nemanja0@users.noreply.github.com>
2022-11-01 17:02:38 +0100

    21 test issue vhdl (#24)
    * Add files via upload
    
    * I kolo u VHDLu
    
    * Add files via upload

---
+++ Nemanja0/test.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity test is
+	port
+	(
+		a, b: in std_logic;
+		c : out std_logic
+	);
+end test;
+
+architecture test_beh of test is
+begin
+	c <= a and b;
+end test_beh;
