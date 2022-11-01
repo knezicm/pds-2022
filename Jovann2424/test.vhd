6e2467844b654ebe2bcabdf0087357915d864234
Jovann2424 <73527927+Jovann2424@users.noreply.github.com>
2022-11-01 20:33:25 +0100

    Test MUX (#27)
    * Test MUX
    
    * Update test.vhd
    
    * Update test.vhd
    
    * Update test.vhd

---
+++ Jovann2424/test.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity test is
+port (
+
+	input  : in std_logic;
+	output : out std_logic
+	);
+end test;
+
+architecture test_arch of test is
+begin
+	output <= input;
+end test_arch;
