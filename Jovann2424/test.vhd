d04b9c061410f35b3dec70d87707cf70c99b957f
Jovann2424 <73527927+Jovann2424@users.noreply.github.com>
2022-11-01 20:05:56 +0100

    Update test.vhd

--- Jovann2424/test.vhd
+++ Jovann2424/test.vhd
@@ -5,11 +5,11 @@ use ieee.std_logic_1164.all;
 entity test is
 port (
 
-	input  : in std_logic_vector (3 downto 0);
-	sel    : in std_logic_vector (1 downto 0);
+	input  : in std_logic;
 	output : out std_logic
 	);
 end test;
 
 architecture test_arch of test is
+	output <= input;
 end test_arch;
