244054dad0bed74bda4b31b73b51047242b2503a
Emanuela Buganik <109093394+e-buga@users.noreply.github.com>
2022-11-02 19:09:30 +0100

    Issue #9 : Added test files

---
+++ ebuganik/test.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity not_gate is
+port 
+	(
+		a : in std_logic;
+		b : out std_logic
+		
+		);
+end not_gate;
+
+architecture beh_arch of not_gate is 
+begin
+		b <= not a;
+end beh_arch;
+		
