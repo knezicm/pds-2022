9e3ec1a80cedba1ff72159183c2ca6a5cd491d7e
jovanabozic <116508532+jovanabozic@users.noreply.github.com>
2022-11-02 20:59:08 +0100

    predaja testnog vhd fajla (#36)

---
+++ jovanabozic/test.vhd
@@ -0,0 +1,24 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity test is
+
+
+	port
+	(
+		
+		x	: in  std_logic_vector(7 downto 0);
+		
+		y	: out std_logic
+		
+	);
+end test;
+
+
+architecture test_arch of test is
+
+begin
+
+	
+
+end test_arch;
