cf2f4b50c9c2034e5a31ac02b86507c2363ca1cd
Azra Elezović <116368163+ElezovicA@users.noreply.github.com>
2022-11-14 18:21:00 +0100

    Update decoder_2_4.vhd
    Fixing code.

--- assignments/51/decoder_2_4.vhd
+++ assignments/51/decoder_2_4.vhd
@@ -2,17 +2,17 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity decoder_2_4 is
-    port (a : in std_logic_vector(1 downto 0);
-          e : in std_logic;
-          o : out std_logic_vector(3 downto 0)
+    port (i_A : in std_logic_vector(1 downto 0);
+          i_E : in std_logic;
+          o_Y : out std_logic_vector(3 downto 0)
 			 );
 end decoder_2_4;
 
 architecture bhv of decoder_2_4 is
 begin
 
-o(0) <= ( e and (not a(1)) and (not a(0)));
-o(1) <= ( e and (not a(1)) and a(0));
-o(2) <= ( e and a(1) and (not a(0)));
-o(3) <= ( e and a(1) and a(0));
+o_Y(0) <= ( i_E and (not i_A(1)) and (not i_A(0)));
+o_Y(1) <= ( i_E and (not i_A(1)) and i_A(0));
+o_Y(2) <= ( i_E and i_A(1) and (not i_A(0)));
+o_Y(3) <= ( i_E and i_A(1) and i_A(0));
 end bhv;
