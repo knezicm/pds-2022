d13b9ff8e7b0144de5a64a08c7968d277d103b26
jovanabozic <116508532+jovanabozic@users.noreply.github.com>
2022-12-01 16:42:56 +0100

    Update adder.vhd

--- assignments/74/adder.vhd
+++ assignments/74/adder.vhd
@@ -47,8 +47,9 @@ use ieee.numeric_std.all;
 
 entity adder is
   port (
-    a_i, b_i   : in std_logic_vector(15 downto 0);
-    c_o        : out std_logic_vector(15 downto 0));
+    a_i   : in std_logic_vector(15 downto 0);
+    b_i   : in std_logic_vector(15 downto 0);
+    c_o   : out std_logic_vector(15 downto 0));
 end adder;
 
 
