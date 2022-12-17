b22ffbf8c73a5e35754ef9956ffc18826a477db5
Nemanja Cenic <59769533+Nemanja0@users.noreply.github.com>
2022-12-17 14:34:23 +0100

    Issue #103 : Create preamble generator

---
+++ assignments/103/preamble_generator.vhd
@@ -0,0 +1,154 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2022
+-- https://github.com/knezicm/pds-2022/
+-----------------------------------------------------------------------------
+--
+-- unit name: preamble_generator
+--
+-- description:
+--
+--   This file implements preamble generator
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2022 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2022 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
+--! @file
+--! @brief Preamble generator
+-----------------------------------------------------------------------------
+
+--! Use standard library
+library ieee;
+--! Use logic elements
+use ieee.std_logic_1164.all;
+--! Use numeric elements
+use ieee.numeric_std.all;
+
+--! @brief Entity for preamble generator
+--! @details This entity contains clock and reset inputs,
+--! input for triggering preamble generation and output port on which preamble is generated.
+entity preamble_generator is
+  port (
+    clk_i   : in  std_logic; --! Input clock signal
+    rst_i   : in  std_logic; --! It resets the system when set to logic '1'.
+    start_i : in  std_logic; --! It triggers preamble generation when is set to logic '1' for one clock period.
+    data_o  : out std_logic  --! Output signal on which generated preamble sequence appears.
+  );
+end preamble_generator;
+
+--! @brief Architecture definition of the preamble generator
+--! @details This design is realized like a Moore machine with a finite number of states by using look-ahead output method.
+--! When start_i is set to logic '1', a sequence of bits "10101010" appears on the output (data_o).
+architecture arch of preamble_generator is
+  type t_mc_sm is (idle1, idle2, write1, write2, write3, write4, write5, write6, write7, write8); --! States of FSM
+  signal state_reg  : t_mc_sm; --! Current state of register
+  signal state_next : t_mc_sm; --! Next state of register
+  signal out_next   : std_logic; --! Temp signal for data_o_reg
+  signal data_o_reg : std_logic; --! Temp signal for data_o
+begin
+  --! State register
+  process(clk_i, rst_i)
+  begin
+    if rst_i = '1' then
+      state_reg <= idle1;
+    elsif rising_edge(clk_i) then
+      state_reg <= state_next;
+    end if;
+  end process;
+
+  --! Output buffer
+  process(clk_i, rst_i)
+  begin
+    if rst_i = '1' then
+      data_o_reg <= '0';
+    elsif rising_edge(clk_i) then
+      data_o_reg <= out_next;
+    end if;
+  end process;
+
+  --! Next-state logic
+  process(state_reg, start_i)
+  begin
+    case state_reg is
+      when idle1 =>
+        if start_i = '1' then
+          state_next <= idle2;
+        else
+          state_next <= idle1;
+        end if;
+      when idle2 =>
+        if start_i = '0' then
+          state_next <= write1;
+        else
+          state_next <= idle2;
+        end if;
+      when write1 =>
+        state_next <= write2;
+      when write2 =>
+        state_next <= write3;
+      when write3 =>
+        state_next <= write4;
+      when write4 =>
+        state_next <= write5;
+      when write5 =>
+        state_next <= write6;
+      when write6 =>
+        state_next <= write7;
+      when write7 =>
+        state_next <= write8;
+      when write8 =>
+        state_next <= idle1;
+    end case;
+  end process;
+
+  --! Look-ahead output logic
+  process(state_next)
+  begin
+    out_next <= '0';
+    case state_next is
+      when idle1 =>
+      when idle2 =>
+      when write1 =>
+        out_next <= '1';
+      when write2 =>
+        out_next <= '0';
+      when write3 =>
+        out_next <= '1';
+      when write4 =>
+        out_next <= '0';
+      when write5 =>
+        out_next <= '1';
+      when write6 =>
+        out_next <= '0';
+      when write7 =>
+        out_next <= '1';
+      when write8 =>
+        out_next <= '0';
+    end case;
+  end process;
+
+  --! Output
+  data_o <= data_o_reg;
+end arch;
---
+++ assignments/103/preamble_generator_tb.vhd
@@ -0,0 +1,125 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2022
+-- https://github.com/knezicm/pds-2022/
+-----------------------------------------------------------------------------
+--
+-- unit name: preamble_generator_tb
+--
+-- description:
+--
+--   This file implements testbench for preamble generator
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2022 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2022 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity preamble_generator_tb is
+end preamble_generator_tb;
+
+architecture arch of preamble_generator_tb is
+  component preamble_generator
+    port(
+      clk_i   : in  std_logic;
+      rst_i   : in  std_logic;
+      start_i : in  std_logic;
+      data_o  : out std_logic
+    );
+  end component;
+
+  constant c_T : time := 20 ns;
+
+  signal clk_i, rst_i, start_i, data_o : std_logic;
+  signal data_o_pom : std_logic := '0';
+
+  constant c_NUM_OF_CLOCKS : integer := 30;
+  signal i, count : integer := 0; -- loop variable
+begin
+
+  -- uut instantiation
+  uut : preamble_generator port map (
+    clk_i   => clk_i,
+    rst_i   => rst_i,
+    start_i => start_i,
+    data_o  => data_o
+  );
+
+  -- stimulus generator for reset
+  rst_i <= '1', '0' after c_T/2;
+
+  -- stimulus for continous clock
+  process
+  begin
+    clk_i <= '0';
+    wait for c_T/2;
+    clk_i <= '1';
+    wait for c_T/2;
+
+    if i = c_NUM_OF_CLOCKS then
+      wait;
+    else
+      i <= i + 1;
+    end if;
+  end process;
+
+  clk_process : process (clk_i)
+    variable v_start1, v_start2 : std_logic := '0';
+  begin
+    if rising_edge(clk_i) and (rst_i /= '1') then -- avoid reset
+      if i = 2 or i = 1 or i = 12 then
+        start_i <= '1';
+        v_start1 := '1';
+      else
+        start_i <= '0';
+      end if;
+      if v_start1 = '1' and start_i = '0' then
+        v_start1 := '0';
+        v_start2 := '1';
+      end if;
+      if v_start2 = '1' and start_i = '0' then
+        count <= 1;
+        data_o_pom <= '0';
+        v_start2 := '0';
+      end if;
+      if count > 0 and count < 8 and start_i = '0' then
+        if data_o /= data_o_pom then
+          assert false report "incorrect value! Expected: " &
+            std_logic'image(data_o_pom) & ", but got: " &
+            std_logic'image(data_o) severity error;
+        end if;
+        count <= count + 1;
+        data_o_pom <= not data_o_pom;
+      end if;
+      if count = 8 then
+        count <= 0;
+        data_o_pom <= '0';
+      end if;
+    end if;
+  end process clk_process;
+end arch;
