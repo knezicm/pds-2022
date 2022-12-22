3c6ab31a2409b395b06d518a752b754fd19cca1b
jovanabozic <116508532+jovanabozic@users.noreply.github.com>
2022-12-22 22:54:24 +0100

    Update nrzi_encoder_tb.vhd
    -Fixing code style issues

--- assignments/106/nrzi_encoder_tb.vhd
+++ assignments/106/nrzi_encoder_tb.vhd
@@ -37,8 +37,8 @@
 -----------------------------------------------------------------------------
 
 -------------------------------------------------------
---! @file
---! @brief nrzi_encoder_tb   testbench
+--!  @file
+--!  @brief nrzi_encoder_tb   testbench
 -------------------------------------------------------
 
 library ieee;
@@ -46,18 +46,18 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
---! @brief nrzi_encoder_tb is testbench of nrzi_encoder entity 
---! @details Entity nzri_encoder_tb is simple testbench entity  
+--!  @brief nrzi_encoder_tb is testbench of nrzi_encoder entity
+--!  @details Entity nzri_encoder_tb is simple testbench entity
 entity nrzi_encoder_tb is
 end nrzi_encoder_tb;
 
---! @brief  Architecture definitin of nrzi_encoder_tb entity
---! @details This architecture is used to test nrzi_encoder functionality
+--!  @brief  Architecture definitin of nrzi_encoder_tb entity
+--!  @details This architecture is used to test nrzi_encoder functionality
 
 architecture arch of nrzi_encoder_tb is
   component nrzi_encoder is
     port(
-	        clk_i  : in  std_logic;
+           clk_i  : in  std_logic;
            rst_i  : in  std_logic;
            data_i : in  std_logic;
            data_o : out std_logic);
@@ -68,52 +68,52 @@ architecture arch of nrzi_encoder_tb is
   signal test_out        : std_logic;
   signal clk_in          : std_logic;
   signal rst_in          : std_logic;
-  signal i               : integer := 0;
-  signal a               : std_logic_vector( 7 downto 0) :="01011101";
-  signal k               : integer  :=0;
+  signal i               : integer   := 0;
+  signal a               : std_logic_vector( 7 downto 0) := "01011101";
+  signal k               : integer   :=0;
   signal x               : std_logic := '0';
-  constant num_of_clocks : integer := 8;
+  constant num_of_clocks : integer   := 8;
 
 
 begin
 
   ut : nrzi_encoder
     port map (
-	   clk_i  => clk_in,
-		rst_i  => rst_in,
-		data_i => test_in,
-		data_o => test_out);
+      clk_i  => clk_in,
+      rst_i  => rst_in,
+      data_i => test_in,
+      data_o => test_out);
 
-  rst_in <= '1', '0' after 10ns;
+  rst_in <= '1', '0' after 10 ns;
 
   tb1 : process
   begin
-    test_in<=a(k);
-    clk_in <= '0';
-    wait for 10ns;
-    clk_in <= '1';
-    wait for 5ns;
+    test_in <= a(k);
+    clk_in  <= '0';
+    wait for 10 ns;
+    clk_in  <= '1';
+    wait for 5 ns;
     if rst_in='0' then
       if test_in='1' then
         assert test_out=not(x)
           report "Error not x=" & std_logic'image(x) & "test_out=" & std_logic'image(test_out)
           severity error;
-	    else
-         assert test_out=x
-         report "Error x"
-         severity error;
-       end if;
-     end if; 
-     wait for 5ns;
+      else
+        assert test_out=x
+        report "Error x"
+        severity error;
+      end if;
+    end if; 
+    wait for 5 ns;
 
-     if i=num_of_clocks then
-       wait;
-     else
-       i<=i+1;
-       k<=k+1;
-     end if;
+    if i=num_of_clocks then
+      wait;
+    else
+      i <= i+1;
+      k <= k+1;
+    end if;
 
-	x <=test_out; 
+    x <= test_out; 
 
   end process tb1;
 
