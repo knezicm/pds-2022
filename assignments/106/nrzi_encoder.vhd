b26354d94723b0b163ebcdf57841f4c9375f0d9f
Jovana Bozic <jovana.bozic@student.etf.unibl.org>
2022-12-22 21:34:50 +0100

    Issue #106 : Nrzi encoding circuit -Adding entity nrzi_encoder -Adding testbench nrzi_encoder_tb git commit -m Issue

---
+++ assignments/106/nrzi_encoder.vhd
@@ -0,0 +1,118 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2022
+-- https://github.com/knezicm/pds-2022/
+-----------------------------------------------------------------------------
+--
+-- unit name:    MULTI_FUNCTION_ARITMETIC_UNIT
+--
+-- description:
+--
+--   This file describe circuit that can do following operation: +, -, +1, -1 .
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2022 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2022 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
+
+-------------------------------------------------------
+--! @file
+--! @brief nrzi_encoder 
+-------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+--! @brief nrzi_encoder entity (encoder of input signal) 
+--! @details This entity represent Non return to zero Inverted encoder of input signal.
+
+entity nrzi_encoder is
+  port (
+  clk_i  : in  std_logic;   --! Clock input
+  rst_i  : in  std_logic;   --! Reset input
+  data_i : in  std_logic;   --! Input for data signal
+  data_o : out std_logic);  --! Output of nrzi_encoder  
+end nrzi_encoder;
+
+--! @brief  Architecture description of nrzi_encoder entity
+--! @details This architecture represent encoding of input signal . 
+--! @details If the input signal is '0', then output of nrzi_encoder  block will remain same. But if input signal is '1', then output of NRZI block will change state.
+
+
+architecture arch of nrzi_encoder is
+  type nrzi_type is (idle, one, zero);
+
+  signal state_reg  : nrzi_type;
+  signal state_next : nrzi_type;
+  signal tmp_out    : std_logic;
+
+
+begin
+
+  pr1 : process(clk_i, rst_i)
+        begin
+		    if rst_i = '1' then
+			   state_reg <= idle;
+			 elsif (clk_i'event and clk_i='1') then
+			   state_reg <= state_next;
+		    end if;
+		  end process pr1;
+
+  pr2 : process(state_reg, data_i)
+        begin
+		    case state_reg is
+			   when idle =>
+				  if data_i = '1' then
+				    state_next <= one;
+				  else
+				    state_next <= zero;
+				  end if;
+				when one =>
+
+				  if data_i ='1' then
+				   state_next<= zero;
+					else
+					state_next<= one;
+				  end if;
+				when zero =>
+				   if data_i='1' then state_next<= one;
+					else state_next<=zero;
+				end if;
+			 end case;
+			end process pr2;
+
+  pr3 : process(state_reg)
+        begin
+		      data_o<= '0';
+
+			   case state_reg is
+				  when idle =>
+				  when one  => data_o<='1';
+				  when zero => data_o<='0';
+				end case;
+
+		 end process pr3;
+
+end arch;
---
+++ assignments/106/nrzi_encoder_tb.vhd
@@ -0,0 +1,120 @@
+-----------------------------------------------------------------------------
+-- Faculty of Electrical Engineering
+-- PDS 2022
+-- https://github.com/knezicm/pds-2022/
+-----------------------------------------------------------------------------
+--
+-- unit name:     MULTI_FUNCTION_ARITMETIC_UNIT_TB
+--
+-- description:
+--
+--   This file represent testbench  of MULTI_FUNCTION_ARITMETIC_UNIT circuit.
+--
+-----------------------------------------------------------------------------
+-- Copyright (c) 2022 Faculty of Electrical Engineering
+-----------------------------------------------------------------------------
+-- The MIT License
+-----------------------------------------------------------------------------
+-- Copyright 2022 Faculty of Electrical Engineering
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a
+-- copy of this software and associated documentation files (the "Software"),
+-- to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense,
+-- and/or sell copies of the Software, and to permit persons to whom
+-- the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+-- THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+-- ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+-- OTHER DEALINGS IN THE SOFTWARE
+-----------------------------------------------------------------------------
+
+-------------------------------------------------------
+--! @file
+--! @brief nrzi_encoder_tb   testbench
+-------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+--! @brief nrzi_encoder_tb is testbench of nrzi_encoder entity 
+--! @details Entity nzri_encoder_tb is simple testbench entity  
+entity nrzi_encoder_tb is
+end nrzi_encoder_tb;
+
+--! @brief  Architecture definitin of nrzi_encoder_tb entity
+--! @details This architecture is used to test nrzi_encoder functionality
+
+architecture arch of nrzi_encoder_tb is
+  component nrzi_encoder is
+    port(
+	        clk_i  : in  std_logic;
+           rst_i  : in  std_logic;
+           data_i : in  std_logic;
+           data_o : out std_logic);
+  end component;
+
+  constant T : time := 30 ns;
+  signal test_in         : std_logic := '0';
+  signal test_out        : std_logic;
+  signal clk_in          : std_logic;
+  signal rst_in          : std_logic;
+  signal i               : integer := 0;
+  signal a               : std_logic_vector( 7 downto 0) :="01011101";
+  signal k               : integer  :=0;
+  signal x               : std_logic := '0';
+  constant num_of_clocks : integer := 8;
+
+
+begin
+
+  ut : nrzi_encoder
+    port map (
+	   clk_i  => clk_in,
+		rst_i  => rst_in,
+		data_i => test_in,
+		data_o => test_out);
+
+  rst_in <= '1', '0' after 10ns;
+
+  tb1 : process
+  begin
+    test_in<=a(k);
+    clk_in <= '0';
+    wait for 10ns;
+    clk_in <= '1';
+    wait for 5ns;
+    if rst_in='0' then
+      if test_in='1' then
+        assert test_out=not(x)
+          report "Error not x=" & std_logic'image(x) & "test_out=" & std_logic'image(test_out)
+          severity error;
+	    else
+         assert test_out=x
+         report "Error x"
+         severity error;
+       end if;
+     end if; 
+     wait for 5ns;
+
+     if i=num_of_clocks then
+       wait;
+     else
+       i<=i+1;
+       k<=k+1;
+     end if;
+
+	x <=test_out; 
+
+  end process tb1;
+
+end arch;
